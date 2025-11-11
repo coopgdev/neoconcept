ae87cea00e201649d468c50729ccabd74495502d
Cooper Greene <70741560+coopgdev@users.noreply.github.com>
2025-11-11 22:10:47 +1100

    Create inst.bat

---
+++ inst.bat
@@ -0,0 +1,22 @@
+
+:: ONLY FOR WINDOWS 10+ !!!
+:: WILL NOT WORK ON MAC!!!
+
+@echo off
+cls
+echo Loading packages=[cv2,playsound3,pyarrow,geocoder,geopy,shapely,geopandas]
+echo.
+echo.
+pip install opencv-python
+pip install playsound3
+pip install pyarrow
+pip install geocoder
+pip install geopy
+pip install shapely
+pip install geopandas
+echo.
+echo.
+echo Any errors? Try installing from conda.
+echo ----------------------------------------------------------
+echo Done.
+echo.
